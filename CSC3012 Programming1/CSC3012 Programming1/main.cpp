cbaffb846c65f6ceae4ede4b4de8c5f7912f2f80
Pierre Gabory <piergabory@gmail.com>
2019-10-11 10:56:24 -0500

    Xcode Init

---
+++ CSC3012 Programming1/CSC3012 Programming1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CSC3012 Programming1
+//
+//  Created by Pierre Gabory on 11/10/2019.
+//  Copyright © 2019 piergabory. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
